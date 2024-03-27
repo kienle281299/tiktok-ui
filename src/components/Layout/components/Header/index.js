687d6e7bbbdeedb51a4b740ed8f26a580b2f1659
kienle2812 <152872077+kienle2812@users.noreply.github.com>
2024-03-27 14:46:16 +0700

    Bug Fixed

---
+++ src/components/Layout/components/Header/index.js
@@ -0,0 +1,5 @@
+function header() {
+    return <h2>header</h2>;
+}
+
+export default header;
---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+//Layout
+import { HeaderOnly } from '~/components/Layout';
+import Home from '~/pages/Home';
+import Profile from '~/pages/Profile';
+import Following from '~/pages/Following';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+// Public Routers
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+const privateRoutes = [];
+export { publicRoutes, privateRoutes };
