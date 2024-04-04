88b501e94aa69b0bf2977ce4d65756edc4e5e0b7
kienle2812 <152872077+kienle2812@users.noreply.github.com>
2024-04-04 10:49:45 +0700

    Xây Dựng UI Phần Header Dự Án Tiktok #3

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/f758851655c5b1253838b58bfb4bdfa6~c5_300x300.webp?lk3s=a5d48078&x-expires=1712214000&x-signature=iTgP5i%2FfDWwNmjWFVqvApTGtafo%3D"
+                alt="Hoa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
